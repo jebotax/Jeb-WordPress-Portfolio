822b22a4927e51a88f95be14f90db237117ad35a
Jeb <jebworking@gmail.com>
2026-04-12 20:18:13 -0700

    first commit

---
+++ wp-content/plugins/mb-custom-post-type/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wpmetabox/mb-custom-post-type',
+        'pretty_version' => '2.11.3',
+        'version' => '2.11.3.0',
+        'reference' => '6b5344b810f55500d5b60c5d5c05971cd14d3bdf',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpmetabox/mb-custom-post-type' => array(
+            'pretty_version' => '2.11.3',
+            'version' => '2.11.3.0',
+            'reference' => '6b5344b810f55500d5b60c5d5c05971cd14d3bdf',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wpmetabox/support' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6c392347cdb77ed133b8a4227b5ee20db89a5717',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wpmetabox/support',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
